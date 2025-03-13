dee2e318162f7cb9a5cf51bca28f8e21d7425674
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-14 06:21:12 +0700

    feat: add style

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,3 +1,5 @@
+import "./HelloWorld.css";
+
 export default function HelloWorld() {
 	const props = {
 		text: "Hello World from spread syntax",
@@ -11,25 +13,10 @@ export default function HelloWorld() {
 }
 
 function HeaderHelloWorld({ text = "Ups, lupa kasih teks" }) {
-	return (
-		<h1
-			style={{
-				color: "red",
-				backgroundColor: "aqua",
-				fontSize: "50px",
-			}}
-		>
-			{text.toUpperCase()}
-		</h1>
-	);
+	return <h1 className="title">{text.toUpperCase()}</h1>;
 }
 
 function ParagraphHelloWorld() {
 	const text = "Selamat Belajar ReactJS dari Programmer Zaman Now";
-	const style = {
-		fontSize: "20px",
-		color: "blue",
-		backgroundColor: "yellow",
-	};
-	return <p style={style}>{text.toLowerCase()}</p>;
+	return <p className="content">{text.toLowerCase()}</p>;
 }
