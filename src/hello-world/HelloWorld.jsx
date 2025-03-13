b4c98d2823e550c21850d98c1a307e563dc3544a
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-14 06:01:53 +0700

    feat: add JSX

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,8 +1,33 @@
 export default function HelloWorld() {
 	return (
 		<div>
-			<h1>Hello World</h1>
-			<p>Selamat Belajar ReactJS dari Programmer Zaman Now</p>
+			<HeaderHelloWorld />
+			<ParagraphHelloWorld />
 		</div>
 	);
+}
+
+function HeaderHelloWorld() {
+	const text = "Hello World";
+	return (
+		<h1
+			style={{
+				color: "red",
+				backgroundColor: "aqua",
+				fontSize: "50px",
+			}}
+		>
+			{text.toUpperCase()}
+		</h1>
+	);
+}
+
+function ParagraphHelloWorld() {
+	const text = "Selamat Belajar ReactJS dari Programmer Zaman Now";
+	const style = {
+		fontSize: "20px",
+		color: "blue",
+		backgroundColor: "yellow",
+	};
+	return <p style={style}>{text.toLowerCase()}</p>;
 }
