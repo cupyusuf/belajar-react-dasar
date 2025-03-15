97af18c08be21ab95b37aef6526c39568ceb8833
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-15 10:12:13 +0700

    feat: add ternary

--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -1,11 +1,7 @@
 export default function Todo({ text, isCompleted, isDeleted = false }) {
-	if (isDeleted) {
-		return null;
-	} else {
-		return (
-			<li>
-				{text} {isCompleted && "✅"}
-			</li>
-		);
-	}
+	return isDeleted ? null : (
+		<li>
+			{text} {isCompleted && "✅"}
+		</li>
+	);
 }
